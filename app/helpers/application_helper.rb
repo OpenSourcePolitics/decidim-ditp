ea7ed9b5a1d4b55641de79741479badff3d9676b
Armand Fardeau <fardeauarmand@gmail.com>
2022-06-30 10:54:15 +0200

    Add breadcrumb to static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def render_static_page_breadcrumb
+    content_tag :div, class: "row column" do
+      content_tag :div, class: "breadcrumb" do
+        concat(link_to(t("home", scope: "decidim.breadcrumb"), decidim.root_path))
+        concat(link_to(t("pages", scope: "decidim.breadcrumb"), decidim.pages_path))
+        concat(link_to translated_attribute(@topic.title), decidim.page_path(@pages.first)) if @topic
+        concat(link_to translated_attribute(@page.title), decidim.page_path(@page))
+      end
+    end
+  end
 end
---
+++ spec/system/breadcrumb_static_pages_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Breadcrumb Static pages", type: :system do
+  let(:organization) { create(:organization) }
+  let!(:page1) { create(:static_page, :with_topic, organization: organization) }
+
+  before do
+    switch_to_host(organization.host)
+    visit decidim.page_path(page1)
+  end
+
+  context "when visiting a single page with topic" do
+    it "renders a breadcrumb" do
+      within ".breadcrumb" do
+        expect(page1).to have_content "Home"
+        expect(page1).to have_content translated(page1.topic.title)
+        expect(page1).to have_content translated(page1.title)
+      end
+    end
+  end
+
+  context "when visiting a single page without topic" do
+    let!(:page1) { create(:static_page, organization: organization) }
+
+    it "renders a breadcrumb" do
+      within ".breadcrumb" do
+        expect(page1).to have_content "Home"
+        expect(page1).to have_content "Pages"
+        expect(page1).to have_content translated(page1.title)
+      end
+    end
+  end
+end
