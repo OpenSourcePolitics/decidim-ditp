656927d47c49d25ce0147f8abc1ec6ed08c6cd43
paulinebessoles <pauline.bessoles@opensourcepolitics.eu>
2022-07-01 13:35:57 +0200

    Adapt to FR design system

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,24 +3,32 @@
 module ApplicationHelper
   def render_static_page_breadcrumb(topic, page, pages)
     content_tag :div, class: "row column" do
-      content_tag :div, class: "breadcrumb" do
-        concat(link_to(t("home", scope: "decidim.breadcrumb"), decidim.root_path))
-        concat(link_to(t("pages", scope: "decidim.breadcrumb"), decidim.pages_path))
-        concat(link_to(translated_attribute(topic.title), decidim.page_path(pages.first))) if topic
-        concat(link_to(translated_attribute(page.title), decidim.page_path(page)))
+      content_tag :nav, class: "fr-breadcrumb" do
+        content_tag :div, class: "fr-collapse", id: "breadcrumb-pages" do
+          content_tag :ol, class: "fr-breadcrumb__list no-bullet-indented" do
+            concat(content_tag(:li, link_to(t("home", scope: "decidim.breadcrumb"), decidim.root_path)))
+            concat(content_tag(:li, link_to(t("pages", scope: "decidim.breadcrumb"), decidim.pages_path)))
+            concat(content_tag(:li, link_to(translated_attribute(topic.title), decidim.page_path(pages.first)))) if topic
+            concat(content_tag(:li, link_to(translated_attribute(page.title), decidim.page_path(page))))
+          end
+        end
       end
     end
   end
 
   def render_assembly_breadcrumb
     content_tag :div, class: "row column" do
-      content_tag :div, class: "breadcrumb" do
-        concat(link_to(t("home", scope: "decidim.breadcrumb"), decidim.root_path))
-        if current_participatory_space.parent.present?
-          concat(link_to(translated_attribute(current_participatory_space.parent.title), breadcrumb_assembly_path(current_participatory_space.parent)))
+      content_tag :nav, class: "fr-breadcrumb" do
+        content_tag :div, class: "fr-collapse", id: "breadcrumb-pages" do
+          content_tag :ol, class: "fr-breadcrumb__list no-bullet-indented" do
+            concat(content_tag(:li, link_to(t("home", scope: "decidim.breadcrumb"), decidim.root_path)))
+            if current_participatory_space.parent.present?
+              concat(content_tag(:li, link_to(translated_attribute(current_participatory_space.parent.title), breadcrumb_assembly_path(current_participatory_space.parent))))
+            end
+            concat(content_tag(:li, link_to(translated_attribute(current_participatory_space.title), breadcrumb_assembly_path(current_participatory_space))))
+            concat(content_tag(:li, link_to(translated_attribute(current_component.name), Decidim::EngineRouter.main_proxy(current_component).root_path))) if try(:current_component)
+          end
         end
-        concat(link_to(translated_attribute(current_participatory_space.title), breadcrumb_assembly_path(current_participatory_space)))
-        concat(link_to(translated_attribute(current_component.name), Decidim::EngineRouter.main_proxy(current_component).root_path)) if try(:current_component)
       end
     end
   end
