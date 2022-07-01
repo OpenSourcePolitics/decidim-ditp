38db5d55dc44292aa539b723b18f4fe3db635387
Armand Fardeau <fardeauarmand@gmail.com>
2022-07-01 12:02:21 +0200

    Fix offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def render_static_page_breadcrumb
+  def render_static_page_breadcrumb(topic, page, pages)
     content_tag :div, class: "row column" do
       content_tag :div, class: "breadcrumb" do
         concat(link_to(t("home", scope: "decidim.breadcrumb"), decidim.root_path))
         concat(link_to(t("pages", scope: "decidim.breadcrumb"), decidim.pages_path))
-        concat(link_to translated_attribute(@topic.title), decidim.page_path(@pages.first)) if @topic
-        concat(link_to translated_attribute(@page.title), decidim.page_path(@page))
+        concat(link_to(translated_attribute(topic.title), decidim.page_path(pages.first))) if topic
+        concat(link_to(translated_attribute(page.title), decidim.page_path(page)))
       end
     end
   end
@@ -16,10 +16,16 @@ module ApplicationHelper
     content_tag :div, class: "row column" do
       content_tag :div, class: "breadcrumb" do
         concat(link_to(t("home", scope: "decidim.breadcrumb"), decidim.root_path))
-        concat(link_to(translated_attribute(current_participatory_space.parent.title), Decidim::Assemblies::Engine.routes.url_helpers.assembly_path(current_participatory_space.parent))) if current_participatory_space.parent.present?
-        concat(link_to(translated_attribute(current_participatory_space.title), Decidim::Assemblies::Engine.routes.url_helpers.assembly_path(current_participatory_space)))
+        if current_participatory_space.parent.present?
+          concat(link_to(translated_attribute(current_participatory_space.parent.title), breadcrumb_assembly_path(current_participatory_space.parent)))
+        end
+        concat(link_to(translated_attribute(current_participatory_space.title), breadcrumb_assembly_path(current_participatory_space)))
         concat(link_to(translated_attribute(current_component.name), Decidim::EngineRouter.main_proxy(current_component).root_path)) if try(:current_component)
       end
     end
+  end
+
+  def breadcrumb_assembly_path(path)
+    Decidim::Assemblies::Engine.routes.url_helpers.assembly_path(path)
   end
 end
