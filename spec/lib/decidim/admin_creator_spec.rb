7b63c71979ec474c9d72f52f058cb0dad7b0422f
AyakorK <guillaume.moret@yahoo.com>
2023-07-03 11:48:30 +0200

    Add some specs

--- spec/lib/decidim/admin_creator_spec.rb
+++ spec/lib/decidim/admin_creator_spec.rb
@@ -30,6 +30,34 @@ module Decidim
       expect(Decidim::User.last.email).to eq(email)
     end
 
+    context "when email already exists" do
+      let!(:user) { create(:user, email: email) }
+
+      # It does not create a new user but retrieve the existing one
+      it "does not create a new user" do
+        expect { described_class.create!(environment) }.not_to change(Decidim::User, :count)
+        expect(Decidim::User.last.admin).to be(true)
+        expect(Decidim::User.last.nickname).to eq(nickname)
+        expect(Decidim::User.last.organization).to eq(organization)
+        expect(Decidim::User.last.email).to eq(email)
+      end
+    end
+
+    context "when some users had been created since the last update" do
+      let!(:user) { create(:user, email: email) }
+
+      before do
+        create(:user, email: "foo@bar.org")
+        create(:user, email: "john@doe.org")
+      end
+
+      it "retrieves the one with the same email" do
+        create_admin = -> { described_class.create!(environment) }
+        expect(&create_admin).not_to change(Decidim::User, :count)
+        expect(&create_admin).not_to(change { Decidim::User.find_by(email: email).id })
+      end
+    end
+
     context "when organization is missing" do
       let(:organization_id) { nil }
 
--- spec/lib/decidim/system_admin_creator_spec.rb
+++ spec/lib/decidim/system_admin_creator_spec.rb
@@ -19,5 +19,30 @@ module Decidim
       expect { described_class.create!(environment) }.to change(Decidim::System::Admin, :count).by(1)
       expect(Decidim::System::Admin.last.email).to eq(email)
     end
+
+    context "when email already exists" do
+      before do
+        Decidim::System::Admin.create!(email: email, password: password)
+      end
+
+      it "does not create a new system admin and retrieve the previous one" do
+        expect { described_class.create!(environment) }.not_to change(Decidim::System::Admin, :count)
+        expect(Decidim::System::Admin.last.email).to eq(email)
+      end
+    end
+
+    context "when some system admins had been created since the last update" do
+      before do
+        Decidim::System::Admin.create!(email: email, password: password)
+        Decidim::System::Admin.create!(email: "foo@bar.org", password: "decidim123456")
+        Decidim::System::Admin.create!(email: "john@doe.org", password: "decidim123456")
+      end
+
+      it "retrieves the one with the same email" do
+        create_admin = -> { described_class.create!(environment) }
+        expect(&create_admin).not_to change(Decidim::System::Admin, :count)
+        expect(&create_admin).not_to(change { Decidim::System::Admin.find_by(email: email).id })
+      end
+    end
   end
 end
