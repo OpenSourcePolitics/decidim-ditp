cf68cc605e2b45b5d16865ebf4eadd87b89a898f
armandfardeau <fardeauarmand@gmail.com>
2023-07-24 16:06:45 +0200

    fix offenses

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,14 +1,5 @@
 # frozen_string_literal: true
 
-require "sentry-ruby"
+require "decidim_app/sentry_setup"
 
-if Rails.application.secrets.dig(:sentry, :enabled)
-  Sentry.init do |config|
-    config.dsn = Rails.application.secrets.dig(:sentry, :dsn)
-    config.breadcrumbs_logger = [:active_support_logger]
-
-    # To activate performance monitoring, set one of these options.
-    # We recommend adjusting the value in production:
-    config.traces_sample_rate = ENV.fetch("SENTRY_SAMPLE_RATE", 0.5)
-  end
-end
+SentrySetup.init
