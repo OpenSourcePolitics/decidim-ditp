cb54a883cb53f3cef6e5295dbf0b2771ed041e42
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-01-17 16:59:37 +0100

    fix: Update spec

--- spec/lib/tasks/scaleway/storage/migrate_from_local_task_spec.rb
+++ spec/lib/tasks/scaleway/storage/migrate_from_local_task_spec.rb
@@ -3,13 +3,15 @@
 require "spec_helper"
 
 describe "rake scaleway:storage:migrate_from_local", type: :task do
+  before do
+    allow(ActiveStorage::Migrator).to receive(:migrate!).with(:local, :scaleway).and_return(true)
+  end
+
   it "preloads the Rails environment" do
     expect(task.prerequisites).to include "environment"
   end
 
   it "invokes the migrator" do
-    expect(ActiveStorage::Migrator).to receive(:migrate!).with(:local, :scaleway).and_return(true)
-
     task.execute
   end
 end
