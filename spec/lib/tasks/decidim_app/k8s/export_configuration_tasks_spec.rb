21073fee7e1b3f000b8ddb800cd4e80cc682c29f
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-01-17 17:13:55 +0100

    fix: Rake tasks specs

--- spec/lib/tasks/decidim_app/k8s/export_configuration_tasks_spec.rb
+++ spec/lib/tasks/decidim_app/k8s/export_configuration_tasks_spec.rb
@@ -5,14 +5,16 @@ require "spec_helper"
 describe "rake decidim_app:k8s:export_configuration", type: :task do
   let(:image) { "dummy-image" }
 
+  before do
+    allow(DecidimApp::K8s::ConfigurationExporter).to receive(:export!).with(image).and_return(true)
+  end
+
   it "preloads the Rails environment" do
     expect(task.prerequisites).to include "environment"
   end
 
   it "invokes the configuration exporter" do
     with_modified_env IMAGE: image do
-      expect(DecidimApp::K8s::ConfigurationExporter).to receive(:export!).with(image).and_return(true)
-
       task.execute
     end
   end
