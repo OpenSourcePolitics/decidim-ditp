38f2616458b49bba3c3c7e22dbc359def97f473e
Quentin Champenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-01-17 17:42:43 +0100

    fix: Rake tasks specs (#47)
    * fix: Update spec
    
    * fix: Update spec
    
    * fix: Rake tasks specs

--- spec/lib/tasks/decidim_app/k8s/dump_db_tasks_spec.rb
+++ spec/lib/tasks/decidim_app/k8s/dump_db_tasks_spec.rb
@@ -3,13 +3,15 @@
 require "spec_helper"
 
 describe "rake decidim_app:k8s:dump_db", type: :task do
+  before do
+    allow(DecidimApp::K8s::ConfigurationExporter).to receive(:dump_db).and_return(true)
+  end
+
   it "preloads the Rails environment" do
     expect(task.prerequisites).to include "environment"
   end
 
   it "invokes the configuration exporter" do
-    expect(DecidimApp::K8s::ConfigurationExporter).to receive(:dump_db).and_return(true)
-
     task.execute
   end
 end
